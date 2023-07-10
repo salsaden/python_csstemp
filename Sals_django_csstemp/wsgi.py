15a98de97706c66abd85a38361217bad15f47030
salsaden <abaaysalmo@gmail.com>
2023-07-10 09:11:03 +0300

    initial commit

---
+++ Sals_django_csstemp/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Sals_django_csstemp project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Sals_django_csstemp.settings')
+
+application = get_wsgi_application()
